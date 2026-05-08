d8503d730bb813ae0878315826ae0822b86ccfe8
yahal-JINX <robloxquantumxd@gmail.com>
2026-05-08 06:46:24 +0300

    more streamlit ( day , hour , phone number )

--- Get_user_input.py
+++ Get_user_input.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import datetime
 import streamlit as st
 df = pd.read_csv('syn_data.csv')
 CSV_PATH  = "syn_data.csv"   # update path if needed
@@ -26,9 +27,9 @@ with st.form("entry_form"):
         "Tutor / Student": st.selectbox("what are you?",["Tutor","Student"]),
         "Subject":         st.multiselect("Select your subjects", ["Math", "English", "Physics", "Biology", "Chemistry", "History", "Computer Science"]),
         "Online / F2F":    st.selectbox("Face to face or online?",["Online","F2F"]),
-        # "Day":             st.multiselect("select the days" , [
-        # "Hour":            get_input("Hour (e.g. 09:00): "),
-        # "Phone number":    get_input("Phone number (e.g. 555-1234): "),
+        "Day":             st.multiselect("select the days", ["sunday","monday","tuesday","Wednesday","thursday","friday","saturday"]),
+        "Hour":            st.time_input('Select an hour', datetime.time(19, 0)),
+        "Phone number":    st.text_input("phone number :"),
     }
     submitted = st.form_submit_button("Save Entry")
 # ── Append to CSV ─────────────────────────────────────────────────────────────
@@ -42,5 +43,6 @@ if submitted:
     df.to_csv(CSV_PATH, index=False)
     print(f"✓ CSV saved!  ({len(df)} rows)")
     st.success(f"CSV saved!  ({len(df)} rows)")
+    st.balloons()
 
 
