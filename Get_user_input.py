880f5dd34289a93c49a408e6fd0abd53cdb983f4
orlyavner <orly.avner@gmail.com>
2026-05-17 14:06:18 +0300

    streamlit input

--- Get_user_input.py
+++ Get_user_input.py
@@ -2,16 +2,25 @@ import pandas as pd
 import json
 import datetime
 import streamlit as st
-dataf = pd.read_csv('syn_data.csv')
 CSV_PATH = "syn_data.csv"
-M_rows = dataf.shape[0]
 # ── Collect input ─────────────────────────────────────────────────────────────
 st.title("Tutor/Student Entry Form")
+with st.form("ID form"):
+    entry = {
+        "ID": st.text_input("enter id :")
+    }
+    ID_BUTTON = st.form_submit_button("Submit ID ")
+if ID_BUTTON:
+    if not entry["ID"]:
+        st.error("Please write an id")
+    else:
+        df_existing = pd.read_csv(CSV_PATH)
+        if entry["ID"] in df_existing["ID"].values:
+            st.error("user exists please sign-in")
 with st.form("entry_form"):
     col1, col2 = st.columns(2)
     with col1:
         entry = {
-            "ID": st.text_input("Enter ID :"),
             "Name": st.text_input("Enter your name :"),
             "Tutor / Student": st.selectbox("what are you?", ["Tutor", "Student"]),
             "Subject": st.multiselect("Select your subjects",
@@ -27,11 +36,12 @@ with st.form("entry_form"):
     with col1:
         st.title("sign in :")
 
-# ── Append to CSV ─────────────────────────────────────────────────────────────
+
+# ── Append to CSV  ─────────────────────────────────────────────────────────────
 if submitted:
-            if not entry["Subject"] or not entry["Day"]:
+        if not entry["Subject"] or not entry["Day"]:
                 st.error("Please select at least one Subject and one Day.")
-            else:
+        else:
                 # Create DataFrame and explode the lists into separate rows
                 new_entry_df = pd.DataFrame([entry])
                 new_entry_df = new_entry_df.explode('Subject').explode('Day')
