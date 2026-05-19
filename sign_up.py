c10bef9ac2ba02f688ab7cc2eb6a9552c83e0156
sagilivshits-pixel <sagi.livshits@gmail.com>
2026-05-19 13:22:19 +0300

    update

--- sign_up.py
+++ sign_up.py
@@ -1,26 +1,24 @@
 import streamlit as st
-
+import pandas as pd
+dataf = pd.read_csv('syn_data.csv')
+CSV_PATH = "syn_data.csv"
 st.markdown("<h1 style='text-align: center;'>Learny - Sign In</h1>", unsafe_allow_html=True)
 st.write("##")
 
 # טופס התחברות
 with st.form("login_form"):
     user_id_input = st.text_input("Enter ID (9 digits):", max_chars=9)
-    # תיבת הסיסמה חזרה כאן (מוגדרת כסודית עם כוכביות)
-    password_input = st.text_input("Enter Password (Optional):", type="password")
+    password_input = st.text_input("Enter Password :", type="password")
 
     # כפתור התחברות
     login_submitted = st.form_submit_button("Sign In", use_container_width=True)
-
-# ברגע שלוחצים על Sign In - בודקים רק את תעודת הזהות (הסיסמה אופציונלית)
+# ברגע שלוחצים על Sign In
 if login_submitted:
-    if not user_id_input.isdigit() or len(user_id_input.strip()) < 9:
-        st.error("Please enter a valid 9-digit ID.")
-    else:
-        # 1. שומר את ה-ID שהוזן כדי שקובץ החיפוש יקרא אותו אוטומטית מיד
-        st.session_state.user_id = user_id_input.strip()
-        st.session_state.search_done = True
-
-        # 2. משנה את הסיגנל ל-'find' כדי שקובץ האם (start_page) יטען את streamlit_find.py
+    # בדיקת התנאי: האם הקלט מכיל רק ספרות והאורך שלו הוא בדיוק 9 ספרות
+    if user_id_input.isdigit() and len(user_id_input.strip()) == 9 :
+        # אם התנאי מתקיים (נכון) - מעביר אותך ישירות לסטרימליט פינד
         st.session_state.page = 'find'
         st.rerun()
+    else:
+        # אם התנאי לא מתקיים (לא נכון) - מציג הודעה שחייב 9 ספרות ולא מחבר
+        st.error("חייב שיהיה 9 ספרות")
